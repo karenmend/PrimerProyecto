9377910d36e11cdd790b16806559470bfb2478f7
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:15:09 -0700

    Agregar archivos de proyecto.

---
+++ PrimerProyecto/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+     class Persona
+    {
+        //ATRIBUTOS ENCAPSULADOS
+        public int Edad { get; set; } //Forma de encapsulamiento/Una propiedad encapsulada.
+        public string Nombre { get; set; }
+        //ATRIBUTOS ENCAPSULADOS
+
+        public Persona(int edad, string nombre)
+        {
+            this.Edad = edad;
+
+            this.Nombre = nombre;
+        
+        }
+
+        public Persona()
+        {
+            Edad = 0;
+
+            Nombre = "";
+        }
+
+        public void cumplirAno()
+        {
+            Edad++;
+        }
+
+    }
+}
---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = 
+                new Persona(25, "José");
+            Console.WriteLine(persona1.Nombre +
+                " tiene " + persona1.Edad + " años ");
+
+            persona1.cumplirAno();
+            Console.WriteLine("Ahora " +  persona1.Nombre + " tiene " + persona1.Edad + " años.");
+
+            Console.ReadLine();
+        }
+    }
+}
